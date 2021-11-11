e2d59d4cfa8e9e9fec28c3d1faebf37c4ef3095f
Jin Cheng <jincheng411@gmail.com>
2021-11-10 22:02:36 -0500

    styled navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Logo from '../../dist/assets/stackline_logo.svg';
 const Navbar = () => {
   return (
     <div className="navbar">
-
+      <span><img src={Logo} className="navbar_logo"/></span>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,17 @@ const config = {
       {
         test: /\.svg$/,
         use: 'file-loader'
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          }
+        ]
       }
     ]
   },
